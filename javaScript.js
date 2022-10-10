7164d11f607d8bce2ede088325cabcbd8bcb7928
Nhoj <amosinnhoj@gmail.com>
2022-10-10 19:37:23 +0800

    Add the 16x16 grid

---
+++ javaScript.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('.container');
+const plasticContainer = document.querySelector('.plasticContainer');
+function loop(){
+    for(let row = 1; row <= 16; row++){
+    create(row);
+        for(let col = 1; col <= 15; col++){
+        create(col);
+    }}
+}
+
+function create (){
+    const grid3 = document.createElement('div');
+    container.append(grid3)
+    grid3.style.padding = '20px';
+    grid3.style.border = '2px solid black';
+    grid3.classList.add('column');
+}
+
+loop();
