782ff820c31976611ac716dd4efafcf9d925089a
Nhoj <amosinnhoj@gmail.com>
2022-10-16 21:12:54 +0800

    fix grid making

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,10 @@
 const container = document.querySelector('.container');
+
+let num = 16
 function loop(){
-    for(let row = 1; row <= 16; row++){
+    for(let row = 0; row < num; row++){
     create(row);
-        for(let col = 1; col <= 15; col++){
+        for(let col = 1; col < num; col++){
         create(col);
     }}
 }
@@ -10,17 +12,62 @@ function loop(){
 function create (){
     const grid3 = document.createElement('div');
     container.append(grid3)
-    grid3.style.padding = '20px';
-    grid3.style.border = '2px solid black';
+    grid3.style.padding = pad + 'px';
+    grid3.style.border = '1px solid black';
     grid3.classList.add('grid');
 }
+
+let pad = ((960/num)/2)-1;
 
 const box = document.querySelectorAll('.grid');
 container.addEventListener('mouseover',color);
 
 function color(e){
     if(e.target.id !== 'container')
-        e.target.style.backgroundColor = 'black';
+        e.target.style.backgroundColor ='black';
 } //e.target isolates the element in a sea of class
+
+const input = document.querySelector('.input');
+const click = document.createElement('button');
+input.append(click);
+click.textContent = 'Set grid';
+
+
+
+
+
+
+click.addEventListener('click', function(){
+    let num2 = prompt('Set the squares: ');
+    steps();
+    loop2(num2);
+});
+
+function steps(){
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+      }
+    
+}
+
+function loop2(num2){
+    for(let row = 0; row < num2; row++){
+    create2(row);
+        for(let col = 1; col < num2; col++){
+        create2(col);
+    }}
+    // let pad2 = ((960/num2)/2)-1;
+    // const fill = document.getElementsByClassName('grid');
+    // console.log(fill);
+    // fill.style.border = '1px solid red';
+}
+
+function create2 (){
+    const grid3 = document.createElement('div');
+    container.append(grid3)
+    //grid3.style.padding = pad2 + 'px';
+    grid3.style.border = '1px solid black';
+    grid3.classList.add('grid');
+}
 
 loop();
